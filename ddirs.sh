91d2d6a7753b6f1b3e19b15a4b571b34bcd146fe
ericpromislow <ericp@activestate.com>
2014-04-10 15:54:36 -0700

    Make ddirs.sh the main file, and symlink to it from ~

--- ddirs.sh
+++ ddirs.sh
@@ -1,1 +1,47 @@
-/home/ericp/.ddirs
+for i in {1..50} ; do
+  alias d$i="d$i="'`pwd`' # dx: set dx=`current dir`
+  alias d${i}-="d$i="''   # dxx-: clear dxx
+  alias cd$i='cd $d'$i    # cdx: cd to $dx
+done
+
+function d() {
+  for i in {1..30} ; do
+     x=d$i
+     y=${!x}
+     if [ ! -z $y ] ; then
+       echo $i:`basename $y` : `echo $y | sed -e s@$HOME@'~'@`
+     fi
+  done
+}
+
+function dsave() {
+  case $# in
+    0)suffix="" ;;
+    *)suffix="$1" ;;
+  esac
+  if [ ! -d ~/.d ] ; then
+    mkdir ~/.d
+  fi
+  for i in {1..30} ; do
+    x=d$i
+    y=${!x}
+    if [ ! -z $y ] ; then
+      echo 'd'$i'="'$y'"'
+    fi
+  done >| ~/.d/d${suffix}.txt
+}
+
+function dload() {
+    case $# in
+      0)suffix="" ;;
+      *)suffix="$1" ;;
+    esac
+    f=~/.d/d${suffix}.txt
+    g=`grep -v '^d[0-9]*="[^"]*" *$' $f`
+    if [ ! -z "$g" ] ; then
+      echo "Bad lines in $f:"
+      echo $g
+      return
+    fi
+    . $f
+}
